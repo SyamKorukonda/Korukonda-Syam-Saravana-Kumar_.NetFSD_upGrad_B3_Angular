fca4ffa7cafed4dee42b4014e0dc498842c80af9
Korukonda Syam Saravana Kumar <syam261004@gmail.com>
2026-03-17 15:49:15 +0530

    Add files via upload

---
+++ week5/day22/Enum_ex.cs
@@ -0,0 +1,43 @@
+﻿
+// Write a C# program to create a Student class by using Enum variables as members of the class.
+
+
+namespace ConsoleApp8
+{
+    // declares the enum
+    enum StudentGrades
+    {
+        Excellent,
+        Good,
+        Average,
+        Poor
+    }
+
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public StudentGrades Grade { get; set; }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Student obj = new Student();
+            obj.Id = 1023;
+            obj.Name = "Scott";
+            obj.Grade = StudentGrades.Good;
+
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("Id: ", obj.Id);
+            Console.WriteLine("Name: ", obj.Name);
+            Console.WriteLine("Grade: ", obj.Grade);
+
+            Console.ReadLine();
+        }
+    }
+
+}
