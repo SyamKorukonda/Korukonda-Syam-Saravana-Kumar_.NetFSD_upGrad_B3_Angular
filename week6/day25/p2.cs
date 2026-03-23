40ceb280eb6905e0c50898abc2ad8a49530e14a4
Korukonda Syam Saravana Kumar <syam261004@gmail.com>
2026-03-23 16:34:47 +0530

    Add files via upload

---
+++ week6/day25/p2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DiscountApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter Product Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter Product Price: ");
+            double price = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Discount Percentage: ");
+            double discount = Convert.ToDouble(Console.ReadLine());
+
+            // Breakpoint here
+            double finalPrice = price - (price * discount / 100);
+
+            Console.WriteLine("\nProduct: " + name);
+            Console.WriteLine("Final Price: " + finalPrice);
+
+            Console.ReadLine();
+        }
+    }
+}
