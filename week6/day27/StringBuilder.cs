b9bb2a482c3866184a7cc1bff693399f3effd5e1
Korukonda Syam Saravana Kumar <syam261004@gmail.com>
2026-03-25 16:57:18 +0530

    Add files via upload

---
+++ week6/day27/StringBuilder.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Text;
+
+namespace ConsoleApp41
+{
+    class Program
+    {
+        static string conStr;
+
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+            Console.WriteLine("[Using  StringBuilder]");
+            Console.WriteLine("Start Time {0}", DateTime.Now.ToString("T"));
+            for(int i = 0; i < 10; i++)
+            {
+                sb.Append(i);
+            }
+            Console.WriteLine("End Time {0}", DateTime.Now.ToString("T"));
+
+            Console.WriteLine();
+
+            string s = "";
+            Console.WriteLine("[Using  String]");
+            Console.WriteLine("Start Time {0}", DateTime.Now.ToString("T"));
+            for (int i = 0; i < 10; i++)
+            {
+                s=s+i;
+            }
+            Console.WriteLine("End Time {0}", DateTime.Now.ToString("T"));
+
+
+            Console.ReadLine(); 
+        }
+    }
+}
