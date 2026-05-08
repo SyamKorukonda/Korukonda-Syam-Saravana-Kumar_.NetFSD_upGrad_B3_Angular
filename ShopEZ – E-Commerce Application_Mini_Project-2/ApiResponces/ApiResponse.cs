2c1ecebb25bec7662a59b873bcc1c172cab3c608
Korukonda Syam Saravana Kumar <syam261004@gmail.com>
2026-05-08 09:57:31 +0530

    Add files via upload

---
+++ ShopEZ – E-Commerce Application_Mini_Project-2/ApiResponces/ApiResponse.cs
@@ -0,0 +1,37 @@
+﻿namespace WebApplication17.ApiResponse
+{
+    // Generic response wrapper class
+    // Used to standardize all API responses (Success / Error) with success status, message, and data.
+    public class ApiResponse<T>
+    {
+        // Indicates whether the request was successful or not
+
+        public bool Success { get; set; }
+
+        // Message describing the result (success or error)
+
+        public string Message { get; set; }
+
+        // Generic data returned from API (can be any type)
+        public T Data { get; set; }
+
+        //Constructor for Sucess response
+        // Used when API returns data
+        public ApiResponse(T data, string message = "Success")
+        {
+            Success = true;
+            Message = message;
+            Data = data;
+        }
+
+        // Constructor for ERROR response
+        // Used when API fails (no data)
+        public ApiResponse(string message)
+        {
+            Success = false;
+            Message = message;
+            Data = default;
+        }
+
+    }
+}
